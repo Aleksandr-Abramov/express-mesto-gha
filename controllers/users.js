8d522d348a47134795910917fb8085e9c45aa098
Aleksandr-Abramov <ale19850505@gmail.com>
2022-09-10 11:36:04 +0300

    отправка/прохождение ревью 0.6

--- controllers/users.js
+++ controllers/users.js
@@ -33,12 +33,12 @@ const createUser = async (req, res) => {
     const user = await User.create({ name, about, avatar });
     res.status(200).send(user);
   } catch (err) {
-    if (err.errors.name.kind === "minlength"
-     || err.errors.name.kind === "required"
-     || err.errors.name.kind === "maxlength"
-     || err.errors.about.kind === "minlength"
+    if (err.errors.about.kind === "minlength"
      || err.errors.about.kind === "required"
      || err.errors.about.kind === "maxlength"
+     || err.errors.name.kind === "minlength"
+     || err.errors.name.kind === "required"
+     || err.errors.name.kind === "maxlength"
      || err.errors.avatar.kind === "required") {
       res.status(400).send({ message: "Не удалось создать пользователя, данные не корректны", ...err });
       return;
