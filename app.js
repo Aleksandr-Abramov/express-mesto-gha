7285868ce997057ff4634f07fba787d032c2d1ba
Aleksandr-Abramov <ale19850505@gmail.com>
2022-09-20 00:34:04 +0300

    отправка/прохождение ревью 0.24

--- app.js
+++ app.js
@@ -11,11 +11,13 @@ const { NOT404FOUND } = require("./utils/constants");
 
 const app = express();
 const { PORT = 3000 } = process.env;
-
 app.use((req, res, next) => {
   req.user = {
-    _id: '5d8b8592978f8bd833ca8133'
+    _id: "5d8b8592978f8bd833ca8133",
   };
+
+  next();
+});
 
 app.use(express.json());
 app.use(cookieParser());
