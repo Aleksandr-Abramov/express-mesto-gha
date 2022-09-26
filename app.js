fa1ef938d0e216361f7e9700a862e4a44b7d133d
Aleksandr-Abramov <ale19850505@gmail.com>
2022-09-26 17:12:42 +0300

    add req/err.log

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const routerUsers = require("./routes/users");
 const routerCards = require("./routes/cards");
 const errorHendler = require("./middlewares/errorHendler");
 const auth = require("./middlewares/auth");
+const { requestLogger, errorLogger } = require("./middlewares/logger");
 
 const Not404Found = require("./utils/errors/Not404Found");
 
@@ -15,14 +16,18 @@ const { PORT = 3000 } = process.env;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(requestLogger);
 
 app.use("/", routerUsers);
 app.use("/", routerCards);
 app.use("/*", auth, (req, res, next) => {
   next(new Not404Found("ошибка 404, страницы не существует"));
 });
+
+app.use(errorLogger);
 app.use(errors());
 app.use(errorHendler);
+
 async function main() {
   await mongoose.connect("mongodb://localhost:27017/mestodb", {});
   await app.listen(PORT);
